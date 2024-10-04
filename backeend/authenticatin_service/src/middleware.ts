dbb0437de4a5d1067caa4da0ee2bc20de64858a8
lakshaykaushik5 <74808038+lakshaykaushik5@users.noreply.github.com>
2024-10-04 16:55:30 +0530

    Update middleware.ts

--- backeend/authenticatin_service/src/middleware.ts
+++ backeend/authenticatin_service/src/middleware.ts
@@ -1,1 +1,18 @@
+import jwt from "jsonwebtokn";
 
+
+const verifyToken = (req,res,next) => {
+  const = req.cookies.token;
+
+  if(!token){
+    return res.status(403).json({msg:"No token provided"});
+  }
+  
+  try{
+    const decoded = jwt.verify(token,SECRET_KEY);
+    req.user = decoded;
+    next();
+  }catch(err){
+    console.log(err);
+  }
+}
